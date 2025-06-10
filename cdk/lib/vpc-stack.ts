36352141422ffebbfe5bf0ac3cc7e6b29700e5ad
Ruslan Kachkovskyi <ruslan.kachkovskyi@automat-it.com>
2025-06-10 11:47:28 +0200

    Use specific CIDR sizes

--- cdk/lib/vpc-stack.ts
+++ cdk/lib/vpc-stack.ts
@@ -1,34 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class VpcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const azs = ["a", "b", "c"].map(az => `${props?.env?.region}${az}`);
+        const azs = ["a", "b", "c"].map(az => `${props?.env?.region}${az}`);
 
-    const vpc = new ec2.Vpc(this, 'TheVPC', {
-      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-      availabilityZones: azs,
-      gatewayEndpoints: {
-        s3: { service: ec2.GatewayVpcEndpointAwsService.S3 },
-        dynamodb: { service: ec2.GatewayVpcEndpointAwsService.DYNAMODB }
-      },
-      flowLogs: {
-        all: {
-          trafficType: ec2.FlowLogTrafficType.REJECT,
-          destination: ec2.FlowLogDestination.toCloudWatchLogs()
-        },
-        reject: {
-          trafficType: ec2.FlowLogTrafficType.ALL,
-          destination: ec2.FlowLogDestination.toS3()
-        }
-      },
-      natGateways: azs.length
-    })
+        const vpc = new ec2.Vpc(this, 'TheVPC', {
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+            availabilityZones: azs,
+            subnetConfiguration: [{
+                name: "public-",
+                subnetType: ec2.SubnetType.PUBLIC,
+                cidrMask: 24
+            }, {
+                name: "private-",
+                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                cidrMask: 24
+            }],
+            gatewayEndpoints: {
+                s3: {service: ec2.GatewayVpcEndpointAwsService.S3},
+                dynamodb: {service: ec2.GatewayVpcEndpointAwsService.DYNAMODB}
+            },
+            flowLogs: {
+                all: {
+                    trafficType: ec2.FlowLogTrafficType.REJECT,
+                    destination: ec2.FlowLogDestination.toCloudWatchLogs()
+                },
+                reject: {
+                    trafficType: ec2.FlowLogTrafficType.ALL,
+                    destination: ec2.FlowLogDestination.toS3()
+                }
+            },
+            natGateways: azs.length
+        })
 
-    vpc.addInterfaceEndpoint("ECR.DKR", {service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER});
-    vpc.addInterfaceEndpoint("ECR.API", {service: ec2.InterfaceVpcEndpointAwsService.ECR});
-  }
+        vpc.addInterfaceEndpoint("ECR.DKR", {service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER});
+        vpc.addInterfaceEndpoint("ECR.API", {service: ec2.InterfaceVpcEndpointAwsService.ECR});
+    }
 }
